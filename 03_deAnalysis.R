f79fbee7f73cff39127ce7de750a1c0494b81b0a
Umar Niazi <uhkniazi@gmail.com>
2018-09-10 14:37:58 +0100

    added individual variance terms for each batch of genes

--- 03_deAnalysis.R
+++ 03_deAnalysis.R
@@ -9,26 +9,25 @@ source('header.R')
 ## load the data
 library(RMySQL)
 
-# db = dbConnect(MySQL(), user='rstudio', password='12345', dbname='Projects', host='127.0.0.1')
-# dbListTables(db)
-# 
-# # get the query
-# g_did
-# q = paste0('select MetaFile.* from MetaFile
-#            where (MetaFile.idData = 28) AND (MetaFile.comment like "%count%")')
-# dfSample = dbGetQuery(db, q)
-# dfSample
-# n = paste0(dfSample$location, dfSample$name)
-# load(n)
-# 
-# ## load the metadata i.e. covariates
-# q = paste0('select Sample.* from Sample where Sample.idData = 28')
-# dfSample = dbGetQuery(db, q)
-# dim(dfSample)
-# dfSample
-# # close connection after getting data
-# dbDisconnect(db)
-load(file.choose())
+db = dbConnect(MySQL(), user='rstudio', password='12345', dbname='Projects', host='127.0.0.1')
+dbListTables(db)
+
+# get the query
+g_did
+q = paste0('select MetaFile.* from MetaFile
+           where (MetaFile.idData = 30) AND (MetaFile.comment like "%count%")')
+dfSample = dbGetQuery(db, q)
+dfSample
+n = paste0(dfSample$location, dfSample$name)
+load(n)
+
+## load the metadata i.e. covariates
+q = paste0('select Sample.* from Sample where Sample.idData = 30')
+dfSample = dbGetQuery(db, q)
+dim(dfSample)
+dfSample
+# close connection after getting data
+dbDisconnect(db)
 
 
 ## make count matrix
@@ -54,10 +53,11 @@ dim(mData)
 ## perform DE analysis
 ## delete sample section after testing
 hist(mData)
+plot(density(mData))
 mData.norm = mData
 # set.seed(123)
-# i = sample(1:nrow(mData.norm), 100, replace = F)
-# dfData = data.frame(t(log(mData.norm[prots,]+0.5)))
+# i = sample(1:nrow(mData.norm), 10, replace = F)
+# dfData = data.frame(t(log(mData.norm[i,]+0.5)))
 
 dfData = data.frame(t(log(mData.norm+0.5)))
 dfData = stack(dfData)
@@ -79,12 +79,12 @@ str(dfData)
 library(lme4)
 fit.lme1 = lmer(values ~ 1  + (1 | Coef) + (1 | Coef.adj1) + (1 | Coef.adj2), data=dfData)
 summary(fit.lme1)
-# fit.lme2 = lmer(values ~ 1 + (1 | Coef) + (1 | Coef.adj1), data=dfData)
-# summary(fit.lme2)
-# fit.lme3 = lmer(values ~ 1 + (1 | Coef) + (1 | Coef.adj2), data=dfData)
-# summary(fit.lme3)
-# fit.lme4 = lmer(values ~ 1 + (1 | Coef), data=dfData)
-# summary(fit.lme4)
+fit.lme2 = lmer(values ~ 1 + (1 | Coef) + (1 | Coef.adj1), data=dfData)
+summary(fit.lme2)
+fit.lme3 = lmer(values ~ 1 + (1 | Coef) + (1 | Coef.adj2), data=dfData)
+summary(fit.lme3)
+fit.lme4 = lmer(values ~ 1 + (1 | Coef), data=dfData)
+summary(fit.lme4)
 
 anova(fit.lme1, fit.lme2, fit.lme3, fit.lme4)
 
@@ -108,7 +108,7 @@ library(rstan)
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
-stanDso = rstan::stan_model(file='tResponse3RandomEffectsNoFixed.stan')
+stanDso = rstan::stan_model(file='tResponse3RandomEffectsMultipleScales.stan')
 
 ## calculate hyperparameters for variance of coefficients
 l = gammaShRaFromModeSD(sd(dfData$values), 2*sd(dfData$values))
@@ -127,28 +127,42 @@ l = gammaShRaFromModeSD(sd(dfData$values), 2*sd(dfData$values))
 # }
 
 ### try a t model without mixture
+## subset the data to get the second level of nested parameters
+## this is done to avoid loops in the stan script to map the scale parameters
+## of each ind/gene to the respective set of coefficients for jitters
+d = dfData[!duplicated(dfData$Coef), ]
+
 lStanData = list(Ntotal=nrow(dfData), Nclusters1=nlevels(dfData$Coef),
                  Nclusters2=nlevels(dfData$Coef.adj1),
                  Nclusters3=nlevels(dfData$Coef.adj2),
+                 NScaleBatches1 = nlevels(dfData$ind), # to add a separate scale term for each gene
                  NgroupMap1=as.numeric(dfData$Coef),
                  NgroupMap2=as.numeric(dfData$Coef.adj1),
                  NgroupMap3=as.numeric(dfData$Coef.adj2),
+                 NBatchMap1=as.numeric(d$ind), # this is where we use the second level mapping
                  y=dfData$values, 
                  gammaShape=l$shape, gammaRate=l$rate)
 
 fit.stan = sampling(stanDso, data=lStanData, iter=1000, chains=3,
                     pars=c('betas', 'sigmaRan1', 'sigmaRan2', 'sigmaRan3',
-                           'nu', 'sigmaPop', 'mu',
+                           'nu', 'sigmaPop', #'mu',
                            'rGroupsJitter1', 'rGroupsJitter2', 'rGroupsJitter3'),
-                    cores=3)#, init=initf, control=list(adapt_delta=0.99, max_treedepth = 12))
+                    cores=3, control=list(adapt_delta=0.99, max_treedepth = 12))#, init=initf, 
+#save(fit.stan, file='temp/fit.stan.mul.scale.10.sep.rds')
 
-print(fit.stan, c('betas', 'sigmaRan1', 'sigmaRan2', 'sigmaRan3', 'sigmaPop', 'nu'), digits=3)
+print(fit.stan, c('betas', 'sigmaRan2', 'sigmaRan3', 'sigmaPop', 'nu'), digits=3)
 traceplot(fit.stan, 'betas')
-traceplot(fit.stan, 'sigmaRan1')
+traceplot(fit.stan, c('sigmaRan2', 'sigmaRan3'))
 
+# m = extract(fit.stan)$mu
+# plot(density(dfData$values))
+# lines(density(colMeans(m)))
+# dim(m)
+# apply(m[sample(1:nrow(m), 100),], 1, function(x) lines(density(x)))
 ## get the coefficient of interest - Modules in our case from the random coefficients section
 mCoef = extract(fit.stan)$rGroupsJitter1
 dim(mCoef)
+#dim(extract(fit.stan)$sigmaRan1)
 ## get the intercept at population level
 iIntercept = as.numeric(extract(fit.stan)$betas)
 # add the intercept to each random effect variable, to get the full coefficient
@@ -179,7 +193,7 @@ levels(d$fBatch)
 ## repeat this for each comparison
 
 ## get a p-value for each comparison
-l = tapply(d$cols, d$split, FUN = function(x, base='Recovered', deflection='Paraplegic') {
+l = tapply(d$cols, d$split, FUN = function(x, base='Pre-Control', deflection='Pre-Recovered') {
   c = x
   names(c) = as.character(d$fBatch[c])
   dif = getDifference(ivData = mCoef[,c[deflection]], ivBaseline = mCoef[,c[base]])
@@ -197,65 +211,29 @@ dfResults$adj.P.Val = p.adjust(dfResults$pvalue, method='BH')
 dfResults$logFC = dfResults$difference
 dfResults$P.Value = dfResults$pvalue
 head(rownames(dfResults))
-library(org.Mm.eg.db)
-## remove X from annotation names
-dfResults$ind = gsub('X', '', as.character(dfResults$ind))
+library(org.Hs.eg.db)
 
-df = AnnotationDbi::select(org.Mm.eg.db, keys = as.character(dfResults$ind), columns = 'SYMBOL', keytype = 'ENTREZID')
-i = match(dfResults$ind, df$ENTREZID)
+df = AnnotationDbi::select(org.Hs.eg.db, keys = as.character(dfResults$ind), columns = 'SYMBOL', keytype = 'UNIPROT')
+i = match(as.character(dfResults$ind), as.character(df$UNIPROT))
 df = df[i,]
 dfResults$SYMBOL = df$SYMBOL
-identical(dfResults$ind, df$ENTREZID)
+identical(as.character(dfResults$ind), as.character(df$UNIPROT))
 ## produce the plots 
-f_plotVolcano(dfResults, 'Bayes: ILC3 vs SIO', fc.lim=c(-3.5, 6.5))
+range(dfResults$logFC)
+f_plotVolcano(dfResults, 'Pre-Recovered vs Pre-Control', fc.lim=range(dfResults$logFC))
 
 m = tapply(dfData$values, dfData$ind, mean)
 i = match(rownames(dfResults), names(m))
 m = m[i]
 identical(names(m), rownames(dfResults))
-plotMeanFC(log(m), dfResults, 0.1, '')
+plotMeanFC((m), dfResults, 0.1, '')
 table(dfResults$pvalue < 0.05)
 table(dfResults$adj.P.Val < 0.05)
 ## save the results 
-write.csv(dfResults, file='results/DEAnalysisILC3VsSIO.xls')
+write.csv(dfResults, file='results/DEAnalysisPre-RecoveredVsPre-Control.xls')
 
-
-
-######### do a comparison with deseq2
-f = factor(dfSample$group1)
-f = relevel(f, 'SIO')
-dfDesign = data.frame(Treatment = f, fAdjust1 = factor(dfSample$group3), fAdjust2=dfSample$fNewBatch,
-                      row.names=colnames(mData))
-
-str(dfDesign)
-oDseq = DESeqDataSetFromMatrix(mData, dfDesign, design = ~ Treatment + fAdjust1 + fAdjust2)
-oDseq = DESeq(oDseq)
-plotDispEsts(oDseq)
-resultsNames(oDseq)
-oRes = results(oDseq, contrast=c('Treatment', 'ILC3', 'SIO'))
-plotMA(oRes)
-temp = as.data.frame(oRes)
-i = match((dfResults$ind), rownames(temp))
-temp = temp[i,]
-identical((dfResults$ind), rownames(temp))
-plot(dfResults$logFC, temp$log2FoldChange, pch=20, main='ILC3: DEseq2 vs Bayes', xlab='Bayes', ylab='DEseq2')
-table(oRes$padj < 0.05)
-write.csv(temp, file='results/DEAnalysisILC3VsSIO_Deseq2.xls')
-
-
-#### plot the deseq2 volcano plot
-dfResults = temp
-dfResults$logFC = log(2^dfResults$log2FoldChange)
-dfResults$P.Value = dfResults$pvalue
-dfResults$adj.P.Val = dfResults$padj
-head(rownames(dfResults))
-## remove X from annotation names
-dfResults$ind = rownames(dfResults)
-
-df = AnnotationDbi::select(org.Mm.eg.db, keys = as.character(dfResults$ind), columns = 'SYMBOL', keytype = 'ENTREZID')
-i = match(dfResults$ind, df$ENTREZID)
-df = df[i,]
-dfResults$SYMBOL = df$SYMBOL
-identical(dfResults$ind, df$ENTREZID)
-## produce the plots 
-f_plotVolcano(dfResults, 'DEseq2: ILC3 vs SIO', fc.lim=c(-4.5, 7))
+s = extract(fit.stan)$sigmaRan1
+dim(s)
+hist(colMeans(s))
+s = colMeans(s)
+names(s) = levels(dfData$ind)
